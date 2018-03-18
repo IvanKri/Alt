b206e62f3c2669eac2efcc76e754d7940a621427
Ivan Kroshko <180bmx360@gmail.com>
2018-03-18 11:21:28 +0200

    last commit

--- BookStorePL/BookStorePL/App_Start/NinjectWebCommon.cs
+++ BookStorePL/BookStorePL/App_Start/NinjectWebCommon.cs
@@ -5,12 +5,13 @@ namespace BookStorePL.App_Start
 {
     using System;
     using System.Web;
+    
 
     using Microsoft.Web.Infrastructure.DynamicModuleHelper;
 
     using Ninject;
     using Ninject.Web.Common;
-
+    
     public static class NinjectWebCommon 
     {
         private static readonly Bootstrapper bootstrapper = new Bootstrapper();
@@ -61,6 +62,7 @@ namespace BookStorePL.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
+            
         }        
     }
 }
